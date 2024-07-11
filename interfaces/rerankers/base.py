d8a778b7c4ca4df73ef276f138cc448d994434df
jamari-morrison <RedCater360@yahoo.com>
2024-07-10 17:48:28 -0700

    add rerank class

---
+++ interfaces/rerankers/base.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from typing import List, Any, Optional
+
+class VectorDB(ABC):
+    @abstractmethod
+    def rerank(self, query: str, docs: List[dict], top_k: int = 5) -> List[dict]:
+        """
+        Rerank and return the top k results from your pipeline
+
+        Args:
+            docs (dict): List of documents with keys "id", "text", and optional key "metadata" to do document specific metadata
+            query (str): The query related to these docs
+            top_k (int): number of results to return
+
+        """
+        pass
+
+    
